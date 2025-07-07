cf23ce571ec9038edaf8a03695fdac5aa267c737
KedarDamale <damalekedar@gmail.com>
2025-07-07 08:13:49 +0530

    added logical  aptitude

---
+++ Others/Logical Reasoning.py
@@ -0,0 +1,154 @@
+"""
+In logical reasoning, "Assertion and Reason" are tools used to evaluate statements and their justifications. Assertions are statements, and reasons explain them. Together, they help determine if a statement is logically supported.
+
+Types Of Assertion and Reasoning Related Questions 
+The Reasoning question in Assertion and reason follows mostly five alternatives. The pattern going to be like the following:
+
+Both the statements: assertion-reason are true, and the reason provided is the right explanation for the assertion statement.
+Both the statements: assertion-reason are true, but the reason which is provided is not correct and does not apply to the assertion statement.
+The assertion statement is true, but the reason is incorrect.
+The reason is true, but the assertion is incorrect.
+Both assertion and reason statements are completely false.
+Also Check: Number Series: Practice, Tricks and more
+
+How To Solve Assertion And Reasoning Questions?
+The following ways are efficient way of solving assertion and reason problems:
+
+1. Understand the Question clearly: Before going to solve the question, you must have the Idea about What is exactly the question. thinking about the problems deeply gives you and Idea about how can solve those problems.
+
+2. Think about each Statement Individually : Break the questions and evaluate each of the statements individually in order to solve those questions.
+
+3. Rule Out the False Options: cutting off the false option is one one of the best way to solve assertion and reason problems.
+
+Things To Consider While Solving Assertion And Reasoning Problems
+Here are some things to consider while addressing assertion and reasoning questions:
+
+Understand The Statements:
+Clearly understand both the assertion and reasoning statements. Break down the information provided and identify the core elements of each statement.
+
+Independently Assess The Assertion And Reasoning:
+Evaluate the truth of the assertion independent of the reasoning statement. Determine if the assertion is true or false based on your existing knowledge.
+
+Examine The Reasoning Statement:
+Evaluate the reasoning statement independently. Check if the reasoning provides a logical explanation or justification for the assertion.
+
+Check For Correctness And Relevance:
+Ensure that both the assertion and reasoning statements are correct and relevant to the given context. An accurate assertion with an incorrect or irrelevant reasoning does not form a valid explanation.
+
+Avoid Assumptions:
+Do not make assumptions beyond the information provided. Base your evaluation solely on the information given in the assertion and reasoning statements.
+
+Consider Scientific Principles:
+For questions related to scientific concepts, apply scientific principles and theories to validate or invalidate the assertion and reasoning.
+
+Examine Cause-And-Effect Relationships:
+Analyze if there is a logical cause-and-effect relationship between the assertion and reasoning. Determine if the reasoning adequately supports the assertion.
+
+Also Check: Logical Games: Logical reasoning question and answers
+
+Assertion and Reason - Solved Examples
+Example 1:
+Assertion (A): Birds have feathers.
+Reason (R): Feathers help birds to fly.
+Options:
+
+A - Both A and R are true, and R is the correct explanation of A.
+B - Both A and R are true, but R is NOT the correct explanation of A.
+C - A is true, but R is false.
+D - A is false, but R is true.
+Answer: A - Both A and R are true, and R is the correct explanation of A.
+
+Explanation: Both the assertion and reason are true. Feathers indeed help birds fly, as they provide lift and control during flight.
+
+
+
+Example 2:
+Assertion (A): The Earth orbits the Sun.
+Reason (R): The Sun is much larger than the Earth.
+Options:
+
+A - Both A and R are true, and R is the correct explanation of A.
+B - Both A and R are true, but R is NOT the correct explanation of A.
+C - A is true, but R is false.
+D - A is false, but R is true.
+Answer: A - Both A and R are true, and R is the correct explanation of A.
+
+Explanation: Both the assertion and reason are true. The Earth's orbit around the Sun is due to the Sun's gravitational pull, which is possible because the Sun is much larger than the Earth.
+
+
+
+Example 3:
+Assertion (A): Plants conduct photosynthesis.
+Reason (R): Photosynthesis is essential for plant growth.
+Options:
+
+A - Both A and R are true, and R is the correct explanation of A.
+B - Both A and R are true, but R is NOT the correct explanation of A.
+C - A is true, but R is false.
+D - A is false, but R is true.
+Answer: A - Both A and R are true, and R is the correct explanation of A.
+
+Explanation: Both the assertion and reason are true. Photosynthesis is a vital process for plant growth and involves the conversion of sunlight into energy.
+
+
+
+Example 4:
+Assertion (A): Water boils at 100 degrees Celsius at sea level.
+Reason (R): Atmospheric pressure affects the boiling point of water.
+Options:
+
+A - Both A and R are true, and R is the correct explanation of A.
+B - Both A and R are true, but R is NOT the correct explanation of A.
+C - A is true, but R is false.
+D - A is false, but R is true.
+Answer: A - Both A and R are true, and R is the correct explanation of A.
+
+Explanation: Both the assertion and reason are true. The boiling point of water varies with changes in atmospheric pressure, and at sea level, it boils at 100 degrees Celsius.
+
+
+
+Example 5:
+Assertion (A): Oxygen is necessary for combustion.
+Reason (R): Combustion is a chemical reaction involving the release of energy.
+Options:
+
+A - Both A and R are true, and R is the correct explanation of A.
+B - Both A and R are true, but R is NOT the correct explanation of A.
+C - A is true, but R is false.
+D - A is false, but R is true.
+Answer: A - Both A and R are true, and R is the correct explanation of A.
+
+Explanation: Both the assertion and reason are true. Oxygen is a crucial component of combustion, which is indeed a chemical reaction releasing energy.
+
+Example 6:
+Assertion (A): Metal objects expand when heated.
+Reason (R): Heating increases the kinetic energy of metal atoms.
+Options:
+
+A - Both A and R are true, and R is the correct explanation of A.
+B - Both A and R are true, but R is NOT the correct explanation of A.
+C - A is true, but R is false.
+D - A is false, but R is true.
+Answer: A - Both A and R are true, and R is the correct explanation of A.
+
+Explanation: Both the assertion and reason are true. Heating metal objects increases the kinetic energy of their atoms, causing expansion.
+
+
+
+Example 7:
+Assertion (A): The Moon has gravitational pull.
+Reason (R): Gravity is a fundamental force of nature.
+Options:
+
+A - Both A and R are true, and R is the correct explanation of A.
+B - Both A and R are true, but R is NOT the correct explanation of A.
+C - A is true, but R is false.
+D - A is false, but R is true.
+Answer: A - Both A and R are true, and R is the correct explanation of A.
+
+Explanation: Both the assertion and reason are true. The Moon's gravitational pull is due to the fundamental force of gravity.
+
+
+
+
+"""
