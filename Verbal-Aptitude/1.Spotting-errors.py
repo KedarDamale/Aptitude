b49631359ddd62abf1735eb00fc0d48ceef31162
KedarDamale <damalekedar@gmail.com>
2025-07-07 07:05:13 +0530

    added verbal aptitude

---
+++ Verbal-Aptitude/1.Spotting-errors.py
@@ -0,0 +1,51 @@
+"""
+Nouns:Names of people, places, things, ideas eg-Kedar
+Pronouns:Replace nouns to avoid repetition. eg-he,who,someone
+Verbs:Express action or state of being eg-eat,ate,eaten,run,ran,etc
+Adjectives:Describe or modify nouns eg-blue, five, that
+Adverbs:Modify verbs, adjectives, or other adverbs. eg-quickly, yesterday, often, very, here
+
+
+Types of sentences
+
+Declarative: Makes a statement → I am a student.
+Interrogative: Asks a question → Are you a student?
+Imperative: Gives a command → Please sit down.
+Exclamatory: Expresses emotion → What a beautiful day!
+
+English Tense Structures
+
+| **Tense**                         | **Affirmative Syntax**           | **Negative Syntax**                  | **Question Syntax**                 |
+| --------------------------------- | -------------------------------- | ------------------------------------ | ----------------------------------- |
+| **1. Present Simple**             | Subject + V1 (+ s/es)            | Subject + do/does not + V1           | Do/Does + Subject + V1?             |
+| **2. Present Continuous**         | Subject + am/is/are + V-ing      | Subject + am/is/are + not + V-ing    | Am/Is/Are + Subject + V-ing?        |
+| **3. Present Perfect**            | Subject + has/have + V3          | Subject + has/have not + V3          | Has/Have + Subject + V3?            |
+| **4. Present Perfect Continuous** | Subject + has/have been + V-ing  | Subject + has/have not been + V-ing  | Has/Have + Subject + been + V-ing?  |
+| **5. Past Simple**                | Subject + V2                     | Subject + did not + V1               | Did + Subject + V1?                 |
+| **6. Past Continuous**            | Subject + was/were + V-ing       | Subject + was/were not + V-ing       | Was/Were + Subject + V-ing?         |
+| **7. Past Perfect**               | Subject + had + V3               | Subject + had not + V3               | Had + Subject + V3?                 |
+| **8. Past Perfect Continuous**    | Subject + had been + V-ing       | Subject + had not been + V-ing       | Had + Subject + been + V-ing?       |
+| **9. Future Simple**              | Subject + will + V1              | Subject + will not + V1              | Will + Subject + V1?                |
+| **10. Future Continuous**         | Subject + will be + V-ing        | Subject + will not be + V-ing        | Will + Subject + be + V-ing?        |
+| **11. Future Perfect**            | Subject + will have + V3         | Subject + will not have + V3         | Will + Subject + have + V3?         |
+| **12. Future Perfect Continuous** | Subject + will have been + V-ing | Subject + will not have been + V-ing | Will + Subject + have been + V-ing? |
+
+| **Tense**                         | **Example Sentence**             | **Marathi Translation**      |
+| --------------------------------- | -------------------------------- | ---------------------------- |
+| **1. Present Simple**             | I eat mangoes.                   | मी आंबे खातो.                |
+| **2. Present Continuous**         | I am eating mangoes.             | मी आंबे खात आहे.             |
+| **3. Present Perfect**            | I have eaten mangoes.            | मी आंबे खाल्ले आहेत.         |
+| **4. Present Perfect Continuous** | I have been eating mangoes.      | मी बरेच वेळ आंबे खात आहे.    |
+| **5. Past Simple**                | I ate mangoes.                   | मी आंबे खाल्ले.              |
+| **6. Past Continuous**            | I was eating mangoes.            | मी आंबे खात होतो.            |
+| **7. Past Perfect**               | I had eaten mangoes.             | मी आंबे खाल्ले होते.         |
+| **8. Past Perfect Continuous**    | I had been eating mangoes.       | मी बऱ्याच वेळ आंबे खात होतो. |
+| **9. Future Simple**              | I will eat mangoes.              | मी आंबे खाईन.                |
+| **10. Future Continuous**         | I will be eating mangoes.        | मी आंबे खात असेन.            |
+| **11. Future Perfect**            | I will have eaten mangoes.       | मी आंबे खाल्लेले असेन.       |
+| **12. Future Perfect Continuous** | I will have been eating mangoes. | मी बऱ्याच वेळ आंबे खात असेन. |
+
+
+
+
+"""
