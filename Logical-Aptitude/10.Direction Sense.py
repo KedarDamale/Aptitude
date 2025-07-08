e78bc1f1ddcb77d934f12a084ec1a8f47b9c065f
KedarDamale <damalekedar@gmail.com>
2025-07-08 20:33:13 +0530

    added logical  aptitude

---
+++ Logical-Aptitude/10.Direction Sense.py
@@ -0,0 +1,37 @@
+"""
+Direction is part of the reasoning in which you identify the direction of a person or an object. Direction can be defined as the direction or path that something is going or is intended to move in, as well as the axis that something is facing or pointing along. The four major directions are north, east, west, and south. There are also sub-main directions that are northeast, southwest, southeast, and northwest. The image below will help you understand this better. 
+
+Points to be remembered before solving Questions on Direction:
+ The angle between North and East, East and South, South, and West, and West and North is 90°.
+The angle between North and northeast, northeast and east, east and southeast, southeast and south, south and southwest, southwest and west, west and northwest, and northwest and north is 45°.
+The clockwise direction is north-east-south-west.
+The anticlockwise direction is north-west-southeast.
+Right means 90° clockwise, and left means 90° anticlockwise.
+ x° Right= x° Clockwise direction, x° Left= x° Anticlockwise direction.
+Note: Some important tip regarding direction
+
+Right of North = east
+Right of east = south
+Right of south = west
+Right of west = north
+Left of north = west
+Less of east = north
+Left of south = east
+Left of west = south.
+Right of northeast = southeast
+Right of southeast = southwest
+Right of southwest = northwest
+Right of northwest = northeast
+Left of northeast = northwest
+Left of southeast = northeast
+Left of southwest = southeast
+Left of northwest = southwest
+Steps to be followed to solve Questions on Direction:
+Always use the 2-D plane to draw a diagram.
+First, make a diagram using the given data.
+Always mark the starting and ending points.
+You should always be careful about taking turns, whether it is right or left.
+Mark the distance along with the path.
+And then you can answer your question easily.
+
+"""
