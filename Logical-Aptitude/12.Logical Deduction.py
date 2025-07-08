62f23418bf2862d01a596a8deaf63a099088759b
KedarDamale <damalekedar@gmail.com>
2025-07-08 20:46:00 +0530

    added logical  aptitude

--- Logical-Aptitude/12.Logical Deduction.py
+++ Logical-Aptitude/12.Logical Deduction.py
@@ -1,3 +1,149 @@
 """
+Question 1
+
+Statements: All birds are animals. Some animals are reptiles.
+
+Conclusions:
+I. Some birds are reptiles.
+II. Some reptiles are animals.
+
+A
+Only conclusion I follows
+
+B
+Only conclusion II follow
+
+C
+Neither I nor II follows
+
+D
+Both I and II follow
+
+Discuss it
+Question 2
+
+Statements: All fruits are foods. Some foods are vegetables.
+
+Conclusions:
+I. All fruits are vegetables.
+II. Some foods are fruits.
+
+A
+Only conclusion I follows
+
+B
+Only conclusion II follows
+
+C
+Neither I nor II follows
+
+D
+Both I and II follow
+
+Discuss it
+Question 3
+
+Statements: Some flowers are roses. All roses are red.
+
+Conclusions:
+I. Some flowers are red.
+II. All flowers are red.
+
+A
+Only conclusion I follows
+
+B
+Only conclusion II follows
+
+C
+Neither I nor II follows
+
+D
+Both I and II follow
+
+Discuss it
+Question 4
+
+Statements: All pencils are pens. Some pens are erasers.
+
+Conclusions:
+I. Some pencils are erasers.
+II. Some pens are pencils.
+
+A
+Only conclusion I follows
+
+B
+Only conclusion II follows
+
+C
+Neither I nor II follows
+
+D
+Both I and II follow
+
+Discuss it
+Question 5
+
+Statements: Some plants are trees. All trees are green.
+
+Conclusions:
+I. Some plants are green.
+II. All plants are green.
+
+A
+Only conclusion I follows
+
+B
+Only conclusion I follows
+
+C
+Neither I nor II follows
+
+D
+Both I and II follow
+
+Discuss it
+Question 6
+
+Statements: All dancers are artists. Some artists are musicians.
+
+Conclusions:
+I. Some dancers are musicians.
+II. Some musicians are artists.
+
+A
+Only conclusion I follows
+
+B
+Only conclusion II follows
+
+C
+Neither I nor II follows
+
+D
+Both I and II follow
+
+Discuss it
+Question 7
+
+Statements: All dogs are pets. No pets are wild animals.
+
+Conclusions:
+I. No dogs are wild animals.
+II. Some dogs are wild animals
+
+A
+Only conclusion I follows
+
+B
+Only conclusion II follows
+
+C
+Neither I nor II follows
+
+D
+Both I and II follow
+
 
 """
