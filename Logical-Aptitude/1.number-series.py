84f549daf475eff1e554732dc867887ba59790ee
KedarDamale <damalekedar@gmail.com>
2025-07-08 20:11:47 +0530

    added logical  aptitude

---
+++ Logical-Aptitude/1.number-series.py
@@ -0,0 +1,13 @@
+"""
+Number Series
+
+A number series is an ordered sequence of numbers arranged according to a specific mathematical rule or pattern. Each number in the series relates to the others through consistent operations like addition, subtraction, multiplication, division, exponents, or more complex relationships.
+
+Number Series is a widely asked topic in the Logical Reasoning section of competitive examinations. There are two main types of questions asked,
+
+1. Missing Term Series: A sequence is given with one or more missing numbers, and you must find the correct number that fits the pattern.
+
+2. Wrong Term Series: A complete sequence is given, but one number is incorrect, and you must identify the wrong term and sometimes correct it.
+
+
+"""
