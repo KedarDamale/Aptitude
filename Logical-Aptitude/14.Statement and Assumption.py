97a2db845dbd5722fd7d91a90b9f727ad7ef5fc1
KedarDamale <damalekedar@gmail.com>
2025-07-08 20:48:25 +0530

    added logical  aptitude

---
+++ Logical-Aptitude/14.Statement and Assumption.py
@@ -0,0 +1,27 @@
+"""
+Statement and Assumption
+Last Updated : 07 Jul, 2025
+Statement and Assumption in Logical Reasoning is the most generally asked topic in competitive exams. The fundamental behind such topics is to test the applicant's critical thinking skills. There are a number of statement and assumption questions asked in the various exams, like SSC, Banking, etc. One should be well-prepared for these topics because these questions are scoring and time-saving as well. 
+Aspirants must practice the given statement and assumption questions. The following article has covered all the concepts and tips, and tricks related to statements and assumptions.
+
+What is a Statement and an Assumption?
+Statement:
+A statement is a piece of information that you are given. It doesn't need proof or assumptions to make sense on its own.
+
+Assumption:
+An assumption is something that is implied or taken for granted in order to make the statement plausible or reasonable. It supports the statement and fills in gaps that are not directly mentioned.
+
+Example:
+
+Statement: The company has decided to give all employees a 10% salary increase.
+Assumption: The company can afford to pay higher salaries. (Implicit - necessary for the statement to make sense)
+Invalid Assumption: All employees will now work harder. ( Unsupported - not necessarily true based on the statement)
+Tips To Solve Statement & Assumption Questions
+Tip 1: Understand the theme of the statement, and do proper screening before concluding to answer the question
+Tip 2: Assumptions will always be Indefinite & Encouraging.
+Tip 3: If the statement contains words like each, only, any, all, every, why, what, these, therefore, but, definitely, and certainly, then the assumption will be false.
+Tip 4: If the statement containing words like many, some, much, and to large extent, then the assumption will be true.
+
+
+
+"""
