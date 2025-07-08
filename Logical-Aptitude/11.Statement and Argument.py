1fa0aa4920df2e030da88a99a62f259f6ec301a2
KedarDamale <damalekedar@gmail.com>
2025-07-08 20:45:19 +0530

    added logical  aptitude

---
+++ Logical-Aptitude/11.Statement and Argument.py
@@ -0,0 +1,25 @@
+"""
+Argument is a very important part of analytical reasoning, expressing different opinions for or against something. In competitive examinations, different types of questions are asked from this section in the form of inferences, assumptions, courses of action, syllogisms, etc. This is why argument is called the backbone of analytical reasoning. 
+
+In this section, we will cover the concept, types of statements and arguments, tips and tricks, and frequently asked questions. Read this article thoroughly to clear your doubts, as certain examples are also given.
+
+What is a Statement and an Argument?
+An argument is an opposing or supporting fact given against a statement. 
+
+In this type of question, certain statements are given which are concerned with an issue, followed by certain arguments in favour or against that statement. 
+
+Generally, both the arguments are contrary to each other and those refer to positive and negative results of the action as mentioned in the statement issued. 
+
+Arguments can be of two types:
+
+Strong Argument: An argument is strong when it follows the exact theme as the statement given. The strong argument gives the practical or real aspect of the argument. It is backed up by the facts and reason depending upon the situation.
+Weak Argument: An argument is called weak if it does not follow the theme of the given statement and it does not address all the given points in the given statements. A weak argument is not related to the real aspects and may be related to a trivial aspect of the statement. 
+Types of statements and Arguments:
+Ambiguous-based arguments: In this type of weak argument, the question does not clarify how the argument is related to the action and what exactly the writer wants to say, therefore the arguments are weak in nature.
+Superfluous arguments:  In this type of weak argument, arguments are superfluous and do not require a deep analysis of the statement.
+Question-back argument: In this type of weak argument, the argument consists of questions asked back by the arguer.
+Established acts: The argument of this type is strong and this is a type of established fact and usually true.
+Experience-based where results follow: Based on past experiences these are strong arguments.
+Prevailing notion of truth: In this type of strong argument, the arguments are universally accepted or true.
+
+"""
