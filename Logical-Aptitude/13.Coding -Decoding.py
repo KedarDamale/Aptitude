689701b02e439a531b6cd14d47705ce363072e3c
KedarDamale <damalekedar@gmail.com>
2025-07-08 20:48:02 +0530

    added logical  aptitude

---
+++ Logical-Aptitude/13.Coding -Decoding.py
@@ -0,0 +1,124 @@
+"""
+Coding -decoding is one of the most common sections included in all competitive exams, which are basically designed for hiring processes and higher education. Many candidates find Coding-Decoding questions challenging to solve, and as a result, they avoid attempting these scoring questions and lose some easy and quick marks.
+
+Coding-Decoding is a reasoning skill where letters, numbers, or symbols are transformed using a hidden rule or pattern. Your task is to crack the logic to encode or decode the given information.
+
+Types of Coding-Decoding
+There are the following types of Coding-Decoding
+
+Letter-based Coding 
+Number-based Coding
+Symbolic-based Coding 
+1. Letter Coding 
+Letter Coding is a type in which the letters are replaced with other letters.
+
+Sample: JUNE is coded as "HSLC", then what should be the code for JULY.
+
+Explanation:
+
+ To solve these kinds of problems, you have to remember that every alphabet has a specific number.
+So, according to the question,
+
+JUNE is coded as "HSLC"
+J is coded as 10, U is coded as 21, N is coded as 14 and E is coded as 5
+In the same way, "HSLC" is coded as
+H is coded as 8, S is coded as 19, L is coded as 12 and C is coded as 3
+
+If you observe both the given word, it is coded by decreasing 2 for each alphabet.
+So, in the same way, to code "JULY", the same number should be decreased.
+So you code "JULY" as:
+J is coded as 10, U is coded as 21, L is coded as 12, Y is coded as 25
+So, in the same way, it decreased each alphabet by 2
+The final solution for JULY would be 8, 19, 10, 23
+
+And you should code the word as "HSJW".
+
+2. Number Coding
+Sample: GEEKS” is coded as 35542, and GAME is coded as 3895, then what would be the code for  SKEEG.
+
+Explanation:
+
+So, here  The code of every letter is already specified in the question itself, so no need to use fixed codes of the letters.
+
+Now, specify the number of each letter to solve the problem. If you observe the two words, some of the letters are repeated, so no need to write the repeated letters.
+
+Now, code the letters.
+
+G is coded as 3, E is coded as 5, K is coded as 4, S is coded as 2, A is coded as 8, and M is coded as 9
+
+Using these codes, "SKEEG" is coded as 2,4,5,5,3. 
+
+3. Symbol Coding
+Sample: If
+
+⋆
+=
+5
+⋆=5
+△
+=
+10
+△=10
+□
+=
+15
+□=15
+⋄
+=
+20
+⋄=20
+then what is the value of 
+⋆
++
+△
++
+⋄
+⋆+△+⋄
+
+Explanation:
+
+Substitute the values:
+
+⋆
+=
+5
+,
+ 
+△
+=
+10
+,
+ 
+⋄
+=
+20
+⋆=5, △=10, ⋄=20
+
+So,
+
+⋆
++
+△
++
+⋄
+⋆+△+⋄ = 5 + 10 + 20 = 35
+
+This decodes the expression 
+⋆
++
+△
++
+⋄
+⋆+△+⋄to 35.
+
+Important Tricks to Solve Coding-Decoding
+Remember the values of A-1, B-2, C-3, D-4, E-5,...............X-24, Y-25, Z-26.
+
+So, finding the reverse value of any number simply subtract that no. from 27 
+
+ Ex:- the reverse of A, i.e., 1 is  27-1 =26 so, our answer is Z, whose value is 26. 
+
+This concept will be very helpful in solving problems of Coding-Decoding.
+
+
+"""
