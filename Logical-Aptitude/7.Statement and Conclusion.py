aa254c4b6a51f18a111d6957c7b285ae52a40f43
KedarDamale <damalekedar@gmail.com>
2025-07-08 20:30:46 +0530

    added logical  aptitude

---
+++ Logical-Aptitude/7.Statement and Conclusion.py
@@ -0,0 +1,11 @@
+"""
+The Statement and Conclusion section is a part of logical and analytical reasoning, which is frequently asked in all competitive exams. Statement and conclusion questions are a set of statements followed by conclusions. The general idea to solve such questions is just to understand the theme of the statement and the context in which the conclusions are made. Candidates need to select the most suitable choice matching the question. Making unnecessary assumptions while solving the statement and conclusion questions is the prime mistake made by aspirants seeking to get selected.
+
+Statement and Conclusion questions are all about the understanding of the statement and its connectivity with the conclusion. The statement is a collection of words needed to construct a meaningful sentence. The question having the statement can have one or two or more than two conclusions; therefore, this requires some tricks and logical thinking for the solution of the statement. 
+
+Tips and Tricks to solve statement & Conclusion Questions
+The first trick is there to look after the keywords/hidden words within the question which are known as qualifiers of the statement.
+There are two qualifiers apart from keywords Definitely and probably based on true and false. Therefore in questions, we have to see what is the conclusion required within the set of questions.
+
+
+"""
